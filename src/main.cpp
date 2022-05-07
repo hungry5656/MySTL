b2bab3ff6de2ddadbcbf02d63c4805fca2ce19a6
hungry5656 <hhhhhenrysun@gmail.com>
2022-05-07 16:28:59 -0700

    init project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by hungry5656 on 5/7/2022.
+//
+#include "type_traits.h"
+
+int main(){
+    return 0;
+}
--- testing/manualTest.cpp
+++ testing/manualTest.cpp
@@ -1,7 +1,3 @@
 //
 // Created by hungry5656 on 5/7/2022.
 //
-
-int main(){
-    return 0;
-}
